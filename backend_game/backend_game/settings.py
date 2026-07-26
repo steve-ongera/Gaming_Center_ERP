94a21c063c1d3cee1bc77fd01f429860b5020214
steve-ongera <gadafisteve001@gmail.com>
2026-07-27 01:49:46 +0300

    update

--- backend_game/backend_game/settings.py
+++ backend_game/backend_game/settings.py
@@ -18,6 +18,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Third party
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'django_filters',
+    'corsheaders',
+    'drf_spectacular',
+    'django_extensions',
+ 
+    # Local
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
