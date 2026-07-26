b24525ad57d4eff640a88904f3686bd7d4432ac1
steve-ongera <gadafisteve001@gmail.com>
2026-07-27 01:48:31 +0300

    update

--- backend_game/api/apps.py
+++ backend_game/api/apps.py
@@ -2,4 +2,9 @@ from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+    verbose_name = 'Gaming Lounge API'
+
+    def ready(self):
+        import api.signals  # noqa: F401  (registers signal receivers)
